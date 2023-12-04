fb8d7480e6b36692edcb04440928d565bdb20b6c
BeastCode06 <sbss3234@gmail.com>
2023-12-04 15:58:41 +0530

    changes to img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { animate, motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
-import me from "../assets/logo_1.png";
+import me from "../assets/img.png";
 
 const Home = () => {
   const clientCount = useRef(null);
